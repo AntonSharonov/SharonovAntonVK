bf090e27b3281bb16b95dd6907c9ea2973cc20b2
AntonSharonov <54035437+AntonSharonov@users.noreply.github.com>
2020-04-08 21:00:50 +0400

    v1.1.4.1
    add shadow and corner radius to friend's avatar

--- SharonovAntonVK/Friends/FriendsTableViewCell.swift
+++ SharonovAntonVK/Friends/FriendsTableViewCell.swift
@@ -11,4 +11,24 @@ import UIKit
 class FriendsTableViewCell: UITableViewCell {
     @IBOutlet weak var friendName: UILabel!
     @IBOutlet weak var friendAvatar: UIImageView!
+    @IBOutlet weak var friendAvatarView: UIView!
+}
+
+final class FriendAvatarView: UIView {
+    
+    override func layoutSubviews() {
+        layer.cornerRadius = bounds.height / 2
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.lightGray.cgColor
+        layer.shadowRadius = 3
+        layer.shadowOpacity = 0.7
+        layer.shadowOffset = CGSize(width: 6, height: 4)
+    }
+    
+}
+
+final class FriendAvatar: UIImageView {
+    override func layoutSubviews() {
+        layer.cornerRadius = bounds.height / 2
+    }
 }
